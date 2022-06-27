7ae18ba5fedf9d55aafdc1d5745ee74b88597f28
alanbenju <benjualan@bitbucket.org>
2022-06-27 00:29:10 -0300

    env variables for db

--- src/db.js
+++ src/db.js
@@ -4,11 +4,11 @@ export let pool;
 
 export async function connectToDB() {
   pool = new Pool({
-    user: 'admin',
-    host: 'localhost',
-    database: 'ping-pong',
-    password: 'admin',
-    port: 35000,
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_DATABASE,
+    password: process.env.DB_PASS,
+    port: process.env.DB_PORT,
   })
 
 }
