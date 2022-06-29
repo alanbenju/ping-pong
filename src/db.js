975b2fbdba627076575ff174027ac215e115f41c
alanbenju <benjualan@bitbucket.org>
2022-06-29 17:53:18 -0300

    test database

--- src/db.js
+++ src/db.js
@@ -1,14 +1,30 @@
 import { Pool } from "pg";
 
-export let pool;
+let pool;
+
+export function getDb(){
+  return pool
+}
 
 export async function connectToDB() {
-  pool = new Pool({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_DATABASE,
-    password: process.env.DB_PASS,
-    port: process.env.DB_PORT,
-  })
+  if (process.env.NODE_ENV == "test"){
+    pool = new Pool({
+      user: process.env.DB_TEST_USER,
+      host: process.env.DB_TEST_HOST,
+      database: process.env.DB_TEST_DATABASE,
+      password: process.env.DB_TEST_PASS,
+      port: process.env.DB_TEST_PORT,
+    })
+  }
+  else{
+    pool = new Pool({
+      user: process.env.DB_USER,
+      host: process.env.DB_HOST,
+      database: process.env.DB_DATABASE,
+      password: process.env.DB_PASS,
+      port: process.env.DB_PORT,
+    })
+  
+  }
 
 }
